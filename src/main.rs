1f1e6992f96f26bdce09f8cf8c1f9d938887c159
Eros Candelaresi <eros@microfocus.com>
2021-11-22 20:50:51 +0100

    Scaffolding for HTSP serializing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+pub mod htsp;
+
+use crate::htsp::HtspSerializer;
 use anyhow::Result;
 use log::*;
+use serde::Serialize;
 
 fn setup_logging() {
     env_logger::init();
@@ -10,6 +14,15 @@ fn setup_logging() {
 async fn main() -> Result<()> {
     setup_logging();
     info!("Hello, world!");
+
+    let test = htsp::Request::Hello {
+        htsp_version: 25,
+        client_name: "FooName",
+        client_version: "1.2.3",
+    };
+    let mut ser = HtspSerializer::create();
+    let res = test.serialize(&mut ser)?;
+    warn!("{:?}", res);
 
     Ok(())
 }
